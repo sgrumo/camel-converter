cdc7c5ddb78294ee78c4cf67388455cd1ef19096
Medici Samuele <samuele.medici@antreem.com>
2019-12-09 15:51:05 +0100

    Ultimate funzionalità

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [image, setImage] = useState("");
+  const random = Math.floor(Math.random() * 10) + 1;
+  const camelImg = require("./camel.svg");
+  const imageArray = Array.from({ length: random }).fill(
+    <img src={camelImg} alt="camel" />
+  );
+
+  const displayImage = image ? (
+    <div className="results-container">
+      <img className="princess" src={image} alt="Princess" />
+      <h4>Results: </h4>
+      <div className="camels-container">{imageArray}</div>
+    </div>
+  ) : (
+    <div className="choose-container">
+      <p>How much is your woman worth?</p>
+      <div className="input-container">
+        <label>
+          Choose the bitch:
+          <input
+            type="file"
+            onChange={e => setImage(URL.createObjectURL(e.target.files[0]))}
+            accept="image/x-png,image/gif,image/jpeg"
+          />
+        </label>
+      </div>
+    </div>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-      <h3>Camel Converter</h3>
+        <h3>Camel Converter</h3>
       </header>
+      {displayImage}
     </div>
   );
 }
